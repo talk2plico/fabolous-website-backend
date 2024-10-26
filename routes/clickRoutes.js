f5199bc3632e321cb0d157d0ba25bad34e418cfe
talk2plico <talk2plico@gmail.com>
2024-10-26 10:00:45 +0100

    Update clickRoutes.js

--- routes/clickRoutes.js
+++ routes/clickRoutes.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const Click = require('../models/Click');
+const { body, validationResult } = require('express-validator');
 
 // Register a click
-router.post('/', async (req, res) => {
-    const { productId, userId } = req.body;
+router.post('/', 
+    [
+        body('productId').isMongoId().withMessage('Invalid product ID'),
+        body('userId').isMongoId().withMessage('Invalid user ID')
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    const click = new Click({
-        productId,
-        userId
-    });
+        const { productId, userId } = req.body;
 
-    try {
-        const savedClick = await click.save();
-        res.status(201).json(savedClick);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
+        const click = new Click({
+            productId,
+            userId
+        });
+
+        try {
+            const savedClick = await click.save();
+            res.status(201).json(savedClick);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
     }
-});
+);
 
 // Get all clicks
 router.get('/', async (req, res) => {
