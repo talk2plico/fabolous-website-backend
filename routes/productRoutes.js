8d2c7247b7aeff28f58307ee5a46a7349d9c5f92
talk2plico <talk2plico@gmail.com>
2024-10-26 10:05:08 +0100

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
-// Get all products
+// Get all products or products with optional search query
 router.get('/', async (req, res) => {
+    const { search } = req.query;
+
+    let query = {};
+    if (search) {
+        query = {
+            $or: [
+                { name: { $regex: search, $options: 'i' } },
+                { description: { $regex: search, $options: 'i' } },
+                { category: { $regex: search, $options: 'i' } }
+            ]
+        };
+    }
+
     try {
-        const products = await Product.find();
+        const products = await Product.find(query);
         res.json(products);
     } catch (err) {
         res.status(500).json({ message: err.message });
